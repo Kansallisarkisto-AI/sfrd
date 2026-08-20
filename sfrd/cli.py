564f82975e9eb8df4b4dcd1a48ee6537f65d92ff
John Mäkelä <29544613+woroko@users.noreply.github.com>
2026-08-20 17:07:11 +0300

    adjust template suggestion defaults

--- sfrd/cli.py
+++ sfrd/cli.py
@@ -173,7 +173,7 @@ def parse_args():
     parser.add_argument(
         "--template_suggestion_minimum",
         type=int,
-        default=3,
+        default=1,
         help="Minimum number of images required in a connected component of the alignment graph for the method to suggest a template."
     )
 
